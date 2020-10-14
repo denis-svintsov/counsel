a9836a0a0c17f4522434df85064cc2c11482212e
dmitriy-svintsov <72872027+dmitriy-svintsov@users.noreply.github.com>
2020-10-14 17:45:47 +0400

    Delete index.js

--- counsel/js/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("trigger").onclick = function() {
-	open()
-};
-
-function open() {
-	document.getElementById("menu").classList.toggle("header-menu_show");
-	document.getElementById("lang").classList.toggle("header-lang_show");
-	document.getElementById("header-menu").classList.toggle("header-menu_show");
-}
